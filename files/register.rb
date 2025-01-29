88778cb7a82dd223b65e6425564a7af2129e4bdb
Daijiro Fukuda <fukuda@clear-code.com>
2025-01-29 12:20:35 +0900

    fix wrong service path

--- files/register.rb
+++ files/register.rb
@@ -1,7 +1,8 @@
 require "win32/service"
 require "win32/registry"
 
-p "register service"
+project_dir = ARGV[0]
+p "register service: #{project_dir}"
 
 service_name = "testservice"
 # Specify ruby bin path
@@ -9,7 +10,7 @@ ruby_path = "C:\\Ruby32-x64\\bin\\ruby.exe"
 
 Win32::Service.create(
   service_name: service_name,
-  binary_path_name: "\"#{ruby_path}\" C:\\test\\run-service.rb",
+  binary_path_name: "\"#{ruby_path}\" \"#{project_dir}service.rb\"",
   display_name: service_name,
 )
 Win32::Service.configure(service_name: service_name, delayed_start: true)
